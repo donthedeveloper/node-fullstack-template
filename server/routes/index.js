e19d34a7ca52c32a02c1890e58c1d9b6ef616bd3
Don <donhansen347@gmail.com>
2018-02-16 23:25:24 -0600

    added index template and rendered in route

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendStatus(200);
+    res.render('index');
 });
 
 module.exports = router;
