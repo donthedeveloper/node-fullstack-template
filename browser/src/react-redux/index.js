052cdd07d69062589eb2d102a2cf0a71f60f480a
Don Hansen <donthedeveloper@users.noreply.github.com>
2019-03-17 13:23:03 -0500

    scss is now properly being compiled with the hot reload

--- browser/src/react-redux/index.js
+++ browser/src/react-redux/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {Route, BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from './store';
+import './index.scss';
 
 import App from './App/App.react';
 
